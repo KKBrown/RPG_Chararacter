d70572fc30a256c1ecec5d7f72e40d6dc5a8e254
Kelroy <wakatta@gmail.com>
2016-02-20 17:53:09 -0500

    Update charCreatorV1.py

--- charCreatorV1.py
+++ charCreatorV1.py
@@ -1,14 +1,7 @@
-# RPG Character Creator
+# RPG Character Creator Version 1
 # Based on and expanded from Charles Wade's original code from August of 2013
 # Edits by Kelroy Brown - KelroyBrown.com - February 2016
 # Learning and Experimenting with Python by doing fun mini projects
-
-# A Character Creator program for a role-playing game. The player should be given a pool of x points (defaulted
-# to 30) to spend on seven primary attributes: Vitality, Strength, Dexterity, Intelligence, Luck and Wisdom.
-# From these seven primary attributes; eight secondary attributes will be generated using various formula. These
-# secondary attributes are Health, Evasion, Physical Defence, Magic Defence, Physical Attack, Magic Attack, Speed and
-# Mana. The player should be able to spend points from the pool on any primary attribute and should also be able to take
-# points from a primary attribute and put them back into the pool. The secondary attributes are automatically generated
 
 # import statements
 
